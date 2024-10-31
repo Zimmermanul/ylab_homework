02bb98e02d0fdb644b8b4d9bd347132279a15264
Max Khabibullin <habibullinmax@gmail.com>
2024-10-31 14:57:12 +0500

    aspectContextFixes

--- habit-tracker/src/main/java/com/mkhabibullin/aspect/AspectContext.java
+++ habit-tracker/src/main/java/com/mkhabibullin/aspect/AspectContext.java
@@ -1,7 +1,6 @@
 package com.mkhabibullin.aspect;
 
 import javax.sql.DataSource;
-import java.util.concurrent.Callable;
 
 public class AspectContext {
   private static final InheritableThreadLocal<Boolean> isTestContext = new InheritableThreadLocal<>();
@@ -19,67 +18,5 @@ public class AspectContext {
   
   public static boolean isTestContext() {
     return Boolean.TRUE.equals(isTestContext.get());
-  }
-  
-  public static DataSource getTestDataSource() {
-    return testDataSource.get();
-  }
-  
-  public static Runnable wrapWithContext(Runnable runnable) {
-    Boolean currentIsTest = isTestContext.get();
-    DataSource currentDataSource = testDataSource.get();
-    return () -> {
-      Boolean previousIsTest = isTestContext.get();
-      DataSource previousDataSource = testDataSource.get();
-      try {
-        if (currentIsTest != null) {
-          isTestContext.set(currentIsTest);
-        }
-        if (currentDataSource != null) {
-          testDataSource.set(currentDataSource);
-        }
-        runnable.run();
-      } finally {
-        if (previousIsTest != null) {
-          isTestContext.set(previousIsTest);
-        } else {
-          isTestContext.remove();
-        }
-        if (previousDataSource != null) {
-          testDataSource.set(previousDataSource);
-        } else {
-          testDataSource.remove();
-        }
-      }
-    };
-  }
-  
-  public static <V> Callable<V> wrapWithContext(Callable<V> callable) {
-    Boolean currentIsTest = isTestContext.get();
-    DataSource currentDataSource = testDataSource.get();
-    return () -> {
-      Boolean previousIsTest = isTestContext.get();
-      DataSource previousDataSource = testDataSource.get();
-      try {
-        if (currentIsTest != null) {
-          isTestContext.set(currentIsTest);
-        }
-        if (currentDataSource != null) {
-          testDataSource.set(currentDataSource);
-        }
-        return callable.call();
-      } finally {
-        if (previousIsTest != null) {
-          isTestContext.set(previousIsTest);
-        } else {
-          isTestContext.remove();
-        }
-        if (previousDataSource != null) {
-          testDataSource.set(previousDataSource);
-        } else {
-          testDataSource.remove();
-        }
-      }
-    };
   }
 }
